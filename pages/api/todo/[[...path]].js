f067d2f306795e1ddde82b1c16dfcaaf508989b1
Natalie <natasha@email.com>
2024-12-13 22:28:47 +0700

    перед установкой TC

--- pages/api/todo/[[...path]].js
+++ pages/api/todo/[[...path]].js
@@ -3,14 +3,27 @@ import { neon } from "@neondatabase/serverless";
 const sql = neon(process.env.DATABASE_URL);
 
 export default async function todo (request, response){
-  const {path} = request.query;
+  const {query, method} = request;
+  const {path} = query;
   const id = path?.[0];
 
+console.log('', method, {id}, request.body);
+switch(method){
+  case 'GET':
+    const rows = await sql `SELECT * FROM todo`;
+    response.status(200).json(rows);
+    return;
+  case 'DELETE':
+    const result = await sql `DELETE FROM todo WHERE id=${id}`;
+    console.log('result=', result);
+    response.status(200).send();
+    return;
+  case 'POST':
+    const resultPost = await sql `INSERT INTO todo (text) values(${request.body.text})`;
+    console.log('resultPost=', resultPost);
+    response.status(201).send();
+    return;
   
-  const rows = await sql `SELECT * FROM todo`;
-  response.status(200).json(
-    rows
-    );
-console.log('****', {id}, {rows});
 
+}
 }
