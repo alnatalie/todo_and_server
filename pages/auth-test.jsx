0cb6d3fc7b5b69006066594c4df95434820a70f8
Natalie <natasha@email.com>
2024-12-21 13:27:58 +0700

    ++GITHUB_AUTH

--- pages/auth-test.jsx
+++ pages/auth-test.jsx
@@ -1,21 +1,27 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Component() {
   const { data: session } = useSession();
-  console.log(' Signed in as', { session });
+  console.log(" Signed in as", { session });
   if (session) {
     return (
-        <>
+      <>
         Signed in as {session.user.name} ({session.user.email}) <br />
-        {session.user.image && <img src={session.user.image} style={{width:'50px',borderRadius:'50%'}}/>}<br />
+        {session.user.image && (
+          <img
+            src={session.user.image}
+            style={{ width: "50px", borderRadius: "50%" }}
+          />
+        )}
+        <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    )
+    );
   }
   return (
     <>
       Not signed in <br />
       <button onClick={() => signIn()}>Sign in</button>
     </>
-  )
+  );
 }
